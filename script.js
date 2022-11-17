b478a17b8e7ae37c9f2f6bbd6a13c5dab7c5f3b6
Damien <damien.deboudt@sfr.fr>
2022-11-17 12:08:22 +0100

    ok

---
+++ script.js
@@ -0,0 +1,16 @@
+let a = 5;
+let b = 6;
+let result = a + b
+console.log(result)
+
+let addition = result + 2;
+console.log(addition);
+
+let soustraction = result - 2;
+console.log(soustraction)
+
+alert(result * addition)
+
+alert(soustraction)
+
+alert(5 % 2)
